02bfc1b0fb3cd789fa97840cffc1b08b4444cf59
Noel Espino Córdova <12452210+slingercode@users.noreply.github.com>
2022-08-09 16:04:32 -0500

    Add arkenfox user-override script

---
+++ user-overrides.js
@@ -0,0 +1,21 @@
+// [SECTION 0400]: SAFE BROWSING (SB)
+user_pref("browser.safebrowsing.malware.enabled", false);
+user_pref("browser.safebrowsing.phishing.enabled", false);
+user_pref("browser.safebrowsing.downloads.enabled", false);
+
+// [SECTION 0800]: LOCATION BAR / SEARCH BAR / SUGGESTIONS / HISTORY / FORMS
+user_pref("keyword.enabled", true);
+user_pref("browser.urlbar.suggest.engines", false);
+
+// [SECTION 1000]: DISK AVOIDANCE
+user_pref("browser.shell.shortcutFavicons", true);
+
+// [SECTION 5000]: OPTIONAL OPSEC
+user_pref("signon.rememberSignons", false);
+user_pref("browser.urlbar.suggest.history", false);
+user_pref("browser.urlbar.suggest.bookmark", false);
+user_pref("browser.urlbar.suggest.openpage", false);
+user_pref("browser.urlbar.suggest.topsites", false);
+
+// [SECTION 9000]: PERSONAL
+user_pref("extensions.pocket.enabled", false);
