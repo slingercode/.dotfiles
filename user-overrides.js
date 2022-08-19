31c96b5a5ff03ac3dd2afaf55ed87d7a58ef99d3
Noel Espino Córdova <github@ednoesco.com>
2022-08-19 15:06:57 -0500

    Disable Firefox RFP and force dark theme

--- user-overrides.js
+++ user-overrides.js
@@ -10,6 +10,9 @@ user_pref("browser.urlbar.suggest.engines", false);
 // [SECTION 1000]: DISK AVOIDANCE
 user_pref("browser.shell.shortcutFavicons", true);
 
+// [SECTION 4500]: RFP (RESIST FINGERPRINTING)
+user_pref("privacy.resistFingerprinting", false);
+
 // [SECTION 5000]: OPTIONAL OPSEC
 user_pref("signon.rememberSignons", false);
 user_pref("browser.urlbar.suggest.history", false);
@@ -18,4 +21,5 @@ user_pref("browser.urlbar.suggest.openpage", false);
 user_pref("browser.urlbar.suggest.topsites", false);
 
 // [SECTION 9000]: PERSONAL
+user_pref("ui.systemUsesDarkTheme", 1);
 user_pref("extensions.pocket.enabled", false);
