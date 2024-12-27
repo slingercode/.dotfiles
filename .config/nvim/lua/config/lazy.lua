bdce27ad60d6dafc23cf66fa326ebd6015b4f50b
Noel Espino Córdova <github@ednoesco.com>
2024-12-27 09:24:27 -0600

    Disable lazyvim init message

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -27,6 +27,6 @@ require("lazy").setup({
     -- colorscheme that will be used when installing plugins.
     install = { colorscheme = { "vesper" } },
     -- automatically check for plugin updates
-    checker = { enabled = true },
+    checker = { enabled = true, notify = false },
 })
 
